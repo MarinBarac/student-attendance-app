45e2cec07e5c9b529a1f8765f883c632e089535d
Marin Barac <marinbarac21@gmail.com>
2024-02-12 19:06:29 +0100

    remove template files

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Student attendance app</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
